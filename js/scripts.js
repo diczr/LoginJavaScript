1b51194d58542e33b301fa4bd6717fe117a56508
João Paulo Nascimento Gardiano <46694429+diczr@users.noreply.github.com>
2019-05-08 23:20:33 -0300

    first commit to loginJS with bootstrap 4
    v.1.0

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+        // Usuários válidos //
+var usuarios = [
+        { 'user': 'joao@joao.com', 'senha' : 123,},
+        { 'user': 'joao@paulo.com', 'senha' : 123,},
+        {'user': 'joao@gardiano.com', 'senha' : 123,}
+]
+        function logIn() {
+                // usuário html 
+            var login = document.getElementById("email").value;
+                // senha html
+            var password = document.getElementById("password").value;   
+                // loop
+            for (a = 0; a < usuarios.length; a++) {
+                // Se login = usuários válidos e password = senhas válidas = logar
+        if (login == usuarios[a].user && password == usuarios[a].senha)
+        {
+          alert(login + ' Bem-Vindo!!')
+          return;  
+        }       
+      } 
+               // caso o usuario e senha não sejam válidos retornar um alerta       
+         alert('Usuário(a) ou senha inválida.')
+    }
